3db801c4bb8b38a730b49bc0291971591d8d234b
Christopher Terrano <mterrano1@yahoo.com>
2022-03-12 11:08:06 -0600

    commit tree 014d7afe1586e68a443a9c4f29fe0e7016cf4eb6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match elements
+        elements.select { |element| element.split("").sort == @word.split("").sort }
+    end
+end
+
+binding.pry
+0
+
